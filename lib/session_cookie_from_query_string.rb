1388424138d71b045002fe5ca13a6f5c7ad1f324
spanner <will@spanner.org>
2010-07-07 10:58:16 +0100

    updated for 0.9.1 and rails 2.3.8 with slightly revised middleware

--- lib/session_cookie_from_query_string.rb
+++ lib/session_cookie_from_query_string.rb
@@ -9,9 +9,7 @@ class SessionCookieFromQueryString
     
     if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
       params = ::Rack::Utils.parse_query(env['QUERY_STRING'])
-      session_key = ActionController::Base.session_options[:session_key]
-      Rails.logger.warn "!! session key is #{session_key}"
-      Rails.logger.warn "!! key param is #{params[session_key]}"
+      session_key = ActionController::Base.session_options[:key] || ActionController::Base.session_options[:session_key]
       env['HTTP_COOKIE'] = [ session_key, params.delete(session_key) ].join('=').freeze unless params[session_key].nil?
     end
     @app.call(env)
